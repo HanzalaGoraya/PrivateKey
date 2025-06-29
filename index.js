ddcbfaffea4059151074757ba31d1d792b934558
HanzalaGoraya <82215073+HanzalaGoraya@users.noreply.github.com>
2025-06-29 17:49:25 +0000

    img

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import secretRouter from "./router.js";
+import cors from "cors";
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Mount the secret router at /api
+app.use("/api", secretRouter);
+app.use(cors);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
