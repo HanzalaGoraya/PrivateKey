0b5e843ffcb5fbe935dace07743b5c6e23690064
HanzalaGoraya <82215073+HanzalaGoraya@users.noreply.github.com>
2025-06-29 21:31:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3001;
 
 // Mount the secret router at /api
 app.use("/api", secretRouter);
-app.use(cors({origin:[*,'http://localhost:3000']}));
+app.use(cors({origin:['*','http://localhost:3000'}));
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
